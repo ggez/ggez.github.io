555afa32b2c947072c2d5c5c981855ad386798a8
Sven-Hendrik Haase <svenstaro@gmail.com>
2017-05-01 19:31:06 +0200

    Progress

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-watchexec --exts md,liquid,scss --restart "sassc style/style.scss style/style.css && cobalt serve"
+watchexec --exts md,liquid,scss,svg,png --restart "sassc style/style.scss style/style.css && cobalt serve"
